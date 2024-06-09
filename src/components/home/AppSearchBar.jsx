0d0ac23d8c89e60dfa7eec54c7e03fb2a1476c20
JiaXiang87 <21301047@bjtu.edu.cn>
2024-06-10 00:11:44 +0800

    将首页搜索框与搜索页连接

--- src/components/home/AppSearchBar.jsx
+++ src/components/home/AppSearchBar.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { SearchBar, ConfigProvider, Toast, Button } from '@nutui/nutui-react';
 import { Photograph } from '@nutui/icons-react';
+import { useNavigate } from 'react-router-dom';
 //import 'nutui/dist/styles/index.scss'; // 导入 NutUI 样式
 import '../../css/AppSearchBar.css';
 
 const AppSearchBar = () => {
+  const navigate = useNavigate();
   return (
-    <div className="search-bar-container" style={{ position: 'fixed', top: 0, left: 0, right: 0, zIndex: 1000, padding: '10px' }}>
+    <div className="search-bar-container" style={{ position: 'fixed', top: 0, left: 0, right: 0, zIndex: 1000, padding: '10px' }} onClick={() => navigate('/search')}>
       <ConfigProvider
         theme={{
           nutuiSearchbarBackground: 'rgba(247, 247, 247, 1)',
@@ -27,7 +29,7 @@ const AppSearchBar = () => {
             </div>
           }
           right={
-            <Button type="primary" className="responsive-button">还没改好</Button>
+            <Button type="primary" className="responsive-button">搜索</Button>
           }
           onSearch={(value) => Toast.show(value)}
         />
