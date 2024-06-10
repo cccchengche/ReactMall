259bd2ed152194328b57a72cd2377e7d7c927319
6ZLeo <136341102+6ZLeo@users.noreply.github.com>
2024-06-10 15:03:41 +0800

    首页轮播帅照，有需要上传照片的dd我
    首页轮播帅照，有需要上传照片的dd我

--- src/components/home/AppSwiper.jsx
+++ src/components/home/AppSwiper.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Swiper, SwiperItem } from '@nutui/nutui-react';
 
 const list = [
-  'https://storage.360buyimg.com/jdc-article/NutUItaro34.jpg',
-  'https://storage.360buyimg.com/jdc-article/NutUItaro2.jpg',
-  'https://storage.360buyimg.com/jdc-article/welcomenutui.jpg',
-  'https://storage.360buyimg.com/jdc-article/fristfabu.jpg'
+  'https://59.110.42.1:8888/down/4IbSeQq3NTHq.jpg',
+  'https://59.110.42.1:8888/down/fJ34qZGUujSn.jpg',
+  'https://59.110.42.1:8888/down/ShtBrtH2WJDz.png',
+  'https://59.110.42.1:8888/down/4IbSeQq3NTHq.jpg'
 ];
 
 const App = () => {
@@ -14,18 +14,28 @@ const App = () => {
       <Swiper
         autoPlay
         loop
-        slideSize={350}
+        slideSize={450}
         indicator
         style={{
           '--nutui-indicator-color': '#426543',
           '--nutui-indicator-dot-color': '#426ddd',
+          width: '100%', // 设置轮播容器的宽度为100%，使其与父容器等宽
+          height: '100%' // 设置轮播容器的高度为100%，使其与父容器等高
         }}
-        className='responsive-image-container'
       >
         {list.map((item, index) => {
           return (
             <SwiperItem key={item}>
-              <img src={list[index]} alt={list[index]} draggable={false} className='responsive-image'/>
+              <img
+                src={list[index]}
+                alt={list[index]}
+                draggable={false}
+                style={{
+                  width: '100%', // 设置图片宽度为100%，使其填充轮播容器
+                  height: '100%', // 设置图片高度为100%，使其填充轮播容器
+                  objectFit: 'cover' // 使用 cover 值，保持图片比例不变且填充整个容器
+                }}
+              />
             </SwiperItem>
           )
         })}
